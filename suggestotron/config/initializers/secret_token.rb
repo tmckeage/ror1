e4e86370a112e4902e103bbea9cc8439735d5eb5
OpenShift guest <tmckeage@gmail.com>
2013-03-01 23:27:11 -0500

    First commit

---
+++ suggestotron/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Suggestotron::Application.config.secret_token = '925ba69444b92fd43eb267ac3ca884ee2aca8b49db42953e5d6c12f0b072b0fd165fc435c796f826e5c1cf28b51eae461ace5a4638128585c0cebc16ab03a2a3'
